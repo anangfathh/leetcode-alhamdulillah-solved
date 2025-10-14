1cae2a4a103a74b6b0867314f0b1d851a8ba2d50
anangfath <anangmuhammad245@gmail.com>
2025-10-14 20:24:51 +0700

    Time: 72 ms (95.72%), Space: 17.9 MB (45.99%) - LeetHub

---
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.py
@@ -0,0 +1,25 @@
+class Solution:
+    def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
+        if k <= 1:
+            return True
+
+        prev_inc = 0
+        curr_inc = 1
+
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                curr_inc += 1
+            else:
+                if prev_inc >= k and curr_inc >= k:
+                    return True
+                elif curr_inc >= 2 * k:
+                    return True
+                prev_inc = curr_inc
+                curr_inc = 1
+
+        if prev_inc >= k and curr_inc >= k:
+            return True
+        if curr_inc >= 2 * k:
+            return True
+
+        return False
